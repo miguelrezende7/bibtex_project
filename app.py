ea7232e5dc3d653fe26bae04704903e728640341
miguelrezende7 <miguelrezende7@gmail.com>
2022-05-22 19:25:27 -0300

    commit heroku

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from resources.bibtex_api import BibtexApi
 from flask import Flask
 from flask_restful import Resource,Api
 
+from resources.test_api import TestApi
+
 app = Flask(__name__)
 api = Api(app)
 
+api.add_resource(TestApi,'/')
 api.add_resource(BibtexApi, '/bibtex')
 
 
